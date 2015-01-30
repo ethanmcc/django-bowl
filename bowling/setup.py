b795c53e59dec145a297e662682c34ef9594766a
Ethan McCreadie <ethanmcc@gmail.com>
2015-01-30 09:40:48 -0500

    tox: Add tox file at root level

---
+++ bowling/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bowling',
+    version='0.1',
+    packages=['score', 'score.migrations', 'bowling'],
+    url='',
+    license='',
+    author='aheld',
+    author_email='aaron.held@nrg.com',
+    description=''
+)
