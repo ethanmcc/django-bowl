f685867d6a084f64a0b1be0d19cd00e472e937ee
Ethan McCreadie <ethanmcc@gmail.com>
2015-02-06 10:12:26 -0500

    Week two

--- bowling/score/models.py
+++ bowling/score/models.py
@@ -1,9 +1,27 @@
-# from django.db import models
+class Game(object):
+    def __init__(self):
+        self.rolls = []
 
-
-class Game():
     def roll(self, pins):
-        pass
+        self.rolls.append(pins)
 
-    def score(self):
-        return -1
+    def is_strike(self, rolls):
+        return rolls[0] == 10
+
+    def is_spare(self, rolls):
+        return sum(rolls[:2]) == 10
+
+    def score(self, rolls=None, frame=1):
+        if rolls is None:
+            rolls = self.rolls
+
+        if frame > 10:
+            return 0
+        frame += 1
+
+        if self.is_strike(rolls):
+            return sum(rolls[:3]) + self.score(rolls[1:], frame)
+        elif self.is_spare(rolls):
+            return sum(rolls[:3]) + self.score(rolls[2:], frame)
+        else:
+            return sum(rolls[:2]) + self.score(rolls[2:], frame)
--- bowling/score/tests.py
+++ bowling/score/tests.py
@@ -5,11 +5,43 @@ from score.models import Game
 
 class BowlingTest(TestCase):
     def setUp(self):
-        pass
+        self.game = Game()
+
+    def roll_many(self, pins, count=20):
+        for roll in range(count):
+            self.game.roll(pins)
+
+    def assert_score(self, score):
+        self.assertEqual(self.game.score(), score)
 
     def test_gutter_game(self):
-        game = Game()
-        for i in range(20):
-            game.roll(0)
+        self.roll_many(0)
+        self.assert_score(0)
 
-        self.assertEqual(game.score(), 0)
+    def test_all_ones(self):
+        self.roll_many(1)
+        self.assert_score(20)
+
+    def test_sum_game(self):
+        self.roll_many(3, 10)
+        self.roll_many(4, 10)
+        self.assert_score(70)
+
+    def test_single_spare(self):
+        self.roll_many(5, 3)
+        self.roll_many(0, 17)
+        self.assert_score(20)
+
+    def test_single_strike(self):
+        self.game.roll(10)
+        self.roll_many(3, 2)
+        self.roll_many(0, 17)
+        self.assert_score(22)
+
+    def test_perfect_game(self):
+        self.roll_many(10, count=12)
+        self.assert_score(300)
+
+    def test_thirteen_strikes(self):
+        self.roll_many(10, count=15)
+        self.assert_score(300)
